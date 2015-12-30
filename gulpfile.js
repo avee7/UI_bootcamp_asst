c78f50333df7ab8041d63ea142d4d57a14be92a4
harshai <harshai@thoughtworks.com>
2015-12-30 15:47:13 +0530

    Update and organize gulpfile to include jade, autoreload and simple server running at port 3100

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,70 @@
-var gulp = require("gulp"),
-    sass = require("gulp-sass"),
-    bourbon = require('node-bourbon');
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    jade = require('gulp-jade'),
+    autoprefixer = require('gulp-autoprefixer'),
+    connect = require('gulp-connect'),
+    plumber = require('gulp-plumber'),
+    del = require('del'),
+    watch = require('gulp-watch'),
+    neat = require('node-neat').includePaths,
+    normalize = require('node-normalize-scss').includePaths;
 
 
-// SASS Related tasks
+gulp.task('clean', function (cb) {
+    return del([
+        './public/**/*'
+    ]);
+});
 
-gulp.task("sass:compile", function () {
-
-    var outputStyle = "compressed"; // "nested" | "compact" | "compressed" | "expanded"
-
-    return gulp.src('./src/sass/**/*.scss')
+gulp.task('css', function () {
+    gulp.src('src/sass/*.scss')
+        .pipe(plumber(''))
+        .pipe(autoprefixer())
         .pipe(sass({
-          outputStyle: outputStyle,
-          includePaths: bourbon.includePaths
+            includePaths: ['styles'].concat(neat).concat(normalize)
         }))
-        .pipe(gulp.dest('./public/css'));
+        .pipe(gulp.dest('./public/css'))
+        .pipe(connect.reload());
 });
 
-gulp.task("sass:watch", function () {
-
-    gulp.watch('./src/sass/main.scss', ["sass:compile"]);
-
-    // gutil.log("[watch:sass]", gutil.colors.yellow("Watching SASS files..."));
-
+gulp.task('html', function () {
+    gulp.src('src/jade/*.jade')
+        .pipe(plumber(''))
+        .pipe(jade({
+            pretty: true
+        }))
+        .pipe(gulp.dest('./public'))
+        .pipe(connect.reload());
 });
 
-// Common tasks
 
-gulp.task("watch", ["sass:watch"]);
-gulp.task("sass", ["sass:compile"]);
+gulp.task('assets', function() {
+    gulp.src(['src/assets/**/*'])
+        .pipe(plumber(''))
+        .pipe(gulp.dest('./public/assets'))
+        .pipe(connect.reload());
+});
+
+gulp.task('watch', function() {
+  gulp.watch('src/sass**/*', ['css']);
+  gulp.watch('src/assets/*', ['assets']);
+  gulp.watch('src/**/*.jade', ['html']);
+  watch('./public/**/*').pipe(connect.reload());
+});
+
+gulp.task('serve', ['build'], function() {
+  connect.server({
+    root: 'public',
+    livereload: true,
+    port: 3100
+  });
+});
+
+gulp.task('deploy', function () {
+    gulp.src(['./public/**/*', './public/*'])
+       .pipe(gulp.dest('./'));
+});
+
+gulp.task('build', ['clean', 'html', 'css', 'assets']);
+
+gulp.task('default', ['clean', 'serve', 'watch']);
